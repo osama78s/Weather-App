655d06d32c045bcf74c75a882b42d031a700a1c4
osama <osamasaif242@gmail.com>
2024-08-09 21:30:21 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import FetchData from './Components/FetchData'
 
 const App = () => {
   return (
-    <div className='h-screen bg-body'>
+    <div className='min-h-screen bg-body'>
       <div className="container px-[20px] flex items-center justify-center h-full">
         <FetchData />
       </div>
